f5aa57194293300ef1ec79e1037bfb0ecfba397b
nicola <nicola.lamattina@outlook.com>
2022-10-11 01:34:27 +0200

    Add model

--- cup/models.py
+++ cup/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from duelist.models import Duelist, Deck
 
-# Create your models here.
+
+class Championship(models.Model):
+    name = models.CharField(max_length=100)
+    duelists = models.ManyToManyField(Duelist)
+    
+class Match(models.Model):
+    player1 = models.ForeignKey(Duelist, on_delete=models.CASCADE, related_name='player1')
+    deck1 = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='deck1')
+    player2 = models.ForeignKey(Duelist, on_delete=models.CASCADE, related_name='player2')
+    deck2 = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='deck2')
+    winner = models.ForeignKey(Duelist, on_delete=models.CASCADE, related_name='winner')
+    status = models.CharField()
+    
--- duelist/models.py
+++ duelist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Deck(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Duelist(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    cosy = models.CharField(max_length=20)
+    decks = models.ManyToManyField(Deck, related_name='players')
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
